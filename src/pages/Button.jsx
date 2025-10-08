371e4ff1a9cfd0bf111c1dc086cf87dccaeda15a
Arif-khan9 <ak4040651@gmail.com>
2025-10-08 13:51:23 +0530

    chnag-btn

--- src/pages/Button.jsx
+++ src/pages/Button.jsx
@@ -6,18 +6,18 @@ const Button = () => {
      <>
      <div className='py-10 flex-col  md:flex-row flex items-center justify-center  md:flex md:justify-center gap-2  '>
         <div className='flex gap-2'>
-         <button className='border px-4 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>ELECTRONICS</button>
-        <button className='border px-4 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>MEN'S CLOTHING</button>
+         <button className='border px-6.5 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>ELECTRONICS</button>
+        <button className='border px-6.5 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>MEN'S CLOTHING</button>
         </div>
         <div className='flex gap-2'>
-         <button className='border px-4 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>JEWELERY</button>
-        <button className='border px-4 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>WOMEN'S CLOTHING</button>
+         <button className='border px-6.5 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>JEWELERY</button>
+        <button className='border px-6.5 md:px-6 lg:px-8 py-3 bg-white text-black hover:bg-blue-950 hover:text-white'>WOMEN'S CLOTHING</button>
 
         </div>
 
      </div>
      <div className='flex justify-center'>
-        <div className='text-white bg-blue-950 py-2 rounded-2xl text-2xl text-center w-80 md:w-180'>SALES UPTO 50%</div>
+        <div className='text-white bg-blue-950 py-2 rounded-2xl text-2xl text-center w-90 md:w-180'>SALES UPTO 50%</div>
         
      </div>
      <div className='flex justify-center mt-10'>
