3a32641e67059005ab8908388e5fb9408d43245f
Arif-khan9 <ak4040651@gmail.com>
2025-10-10 17:13:09 +0530

    api data add

--- src/pages/Electronics.jsx
+++ src/pages/Electronics.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import  tishrt from '../assets/tishrt.jpg'
+import React, { useEffect, useState } from 'react'
+
 
 
 const Electronics = () => {
+
+   const[card , setCard] = useState([])
+
+   const cardFetchapi = async ()=>{
+    try{
+        const res = await fetch("https://e-commerce-backened-4fih.onrender.com/products")
+        const data = await res.json()
+        console.log(data)
+        setCard(data)
+         setCard(data.slice(9, 14))
+    }catch(error){
+        console.log(error, "error")
+    }
+   }
+  useEffect(()=>{
+    cardFetchapi()
+  },[])
+
   return (
     <>
     <div className='mt-10 px-15'>
@@ -21,97 +39,27 @@ const Electronics = () => {
         </div>
     </div>
     <section>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center mt-10 gap-15 px-10 '>
-            <div className=' w-90 bg-amber-200  shadow-2xl'>
-                <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center mt-10 gap-15 px-10 '>
+            {card.map((item , index)=>{
+              return(
+                <div key={index} className=' w-90 rounded-2xl  shadow-2xl'>
+                <img className='h-70 rounded-t-2xl w-full object-contain' src={item.image } alt="" />
                 <div className='px-3'>
-                    <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
+                    <h3 className='text-xl font-bold mt-4 '>{item.category}</h3>
+                <p className='mt-2'>{item.description.slice(0,120)}</p>
                 <div className='flex mt-1 items-center justify-between py-2'>
-                    <p>300$</p>
-                    <div className='flex gap-2'>
+                    <p>${item.price}</p>
+                    <div className='flex py-5 gap-2'>
                         <div className='h-4 w-4 rounded-full bg-amber-900'></div>
                         <div className='h-4 w-4 rounded-full bg-blue-500'></div>
                         <div className='h-4 w-4 rounded-full bg-orange-600'></div>
                     </div>
                 </div>
                 </div>
-            </div>
-            <div className=' w-90 bg-amber-200  shadow-2xl'>
-                <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                <div className='px-3'>
-                    <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                <div className='flex mt-1 items-center justify-between py-2'>
-                    <p>300$</p>
-                    <div className='flex gap-2'>
-                        <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                        <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                        <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div className=' w-90 bg-amber-200  shadow-2xl'>
-                <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                <div className='px-3'>
-                    <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                <div className='flex mt-1 items-center justify-between py-2'>
-                    <p>300$</p>
-                    <div className='flex gap-2'>
-                        <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                        <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                        <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div className=' w-90 bg-amber-200  shadow-2xl'>
-                <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                <div className='px-3'>
-                    <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                <div className='flex mt-1 items-center justify-between py-2'>
-                    <p>300$</p>
-                    <div className='flex gap-2'>
-                        <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                        <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                        <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div className=' w-90 bg-amber-200  shadow-2xl'>
-                <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                <div className='px-3'>
-                    <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                <div className='flex mt-1 items-center justify-between py-2'>
-                    <p>300$</p>
-                    <div className='flex gap-2'>
-                        <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                        <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                        <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div className=' w-90 bg-amber-200  shadow-2xl'>
-                <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                <div className='px-3'>
-                    <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                <div className='flex mt-1 items-center justify-between py-2'>
-                    <p>300$</p>
-                    <div className='flex gap-2'>
-                        <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                        <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                        <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                    </div>
-                </div>
-                </div>
-            </div>
+            </div>  
+              )
+            })}
+            
         </div>
     </section>
     </>
--- src/pages/Womens.jsx
+++ src/pages/Womens.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import tishrt from '../assets/tishrt.jpg'
+import React, { useEffect, useState } from 'react'
+
 const Womens = () => {
+
+     const[card , setCard] = useState([])
+        
+           const cardFetchapi = async ()=>{
+            try{
+                const res = await fetch("https://e-commerce-backened-4fih.onrender.com/products")
+                const data = await res.json()
+                console.log(data)
+                setCard(data)
+                 setCard(data.slice(0, 4))
+            }catch(error){
+                console.log(error, "error")
+            }
+           }
+          useEffect(()=>{
+            cardFetchapi()
+          },[])
   return (
     <>
     <div className='mt-10 px-15'>
-           <p className='text-2xl font-bold text-gray-700 '> WOMEN'S CLOTHING</p>
+           <p className='text-2xl font-bold text-gray-700 '> MEN'S CLOTHING</p>
         </div>
         <div className='flex flex-col lg:flex-row justify-between px-15 mt-10 lg:gap-0 gap-3 '>
             <div className='flex gap-2.5 flex-col lg:flex-row'>
@@ -20,14 +37,17 @@ const Womens = () => {
         </div>
         <section>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center mt-10 gap-15 px-10 '>
-                <div className=' w-90 bg-amber-200  shadow-2xl'>
-                    <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
+                {
+                    card.map((item , index )=>{
+                        return(
+                             <div key={index} className=' w-90 rounded-2xl  shadow-2xl'>
+                    <img className='h-70 rounded-t-2xl w-full object-contain' src={item.image } alt="" />
                     <div className='px-3'>
-                        <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                    <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
+                        <h3 className='text-xl font-bold mt-4'>{item. category}</h3>
+                    <p className='mt-2'>{item.description.slice(0,120)}</p>
                     <div className='flex mt-1 items-center justify-between py-2'>
-                        <p>300$</p>
-                        <div className='flex gap-2'>
+                        <p>${item.price}</p>
+                        <div className='flex py-5 gap-2'>
                             <div className='h-4 w-4 rounded-full bg-amber-900'></div>
                             <div className='h-4 w-4 rounded-full bg-blue-500'></div>
                             <div className='h-4 w-4 rounded-full bg-orange-600'></div>
@@ -35,81 +55,15 @@ const Womens = () => {
                     </div>
                     </div>
                 </div>
-                <div className=' w-90 bg-amber-200  shadow-2xl'>
-                    <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                    <div className='px-3'>
-                        <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                    <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                    <div className='flex mt-1 items-center justify-between py-2'>
-                        <p>300$</p>
-                        <div className='flex gap-2'>
-                            <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                            <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                            <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className=' w-90 bg-amber-200  shadow-2xl'>
-                    <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                    <div className='px-3'>
-                        <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                    <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                    <div className='flex mt-1 items-center justify-between py-2'>
-                        <p>300$</p>
-                        <div className='flex gap-2'>
-                            <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                            <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                            <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className=' w-90 bg-amber-200  shadow-2xl'>
-                    <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                    <div className='px-3'>
-                        <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                    <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                    <div className='flex mt-1 items-center justify-between py-2'>
-                        <p>300$</p>
-                        <div className='flex gap-2'>
-                            <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                            <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                            <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className=' w-90 bg-amber-200  shadow-2xl'>
-                    <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                    <div className='px-3'>
-                        <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                    <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                    <div className='flex mt-1 items-center justify-between py-2'>
-                        <p>300$</p>
-                        <div className='flex gap-2'>
-                            <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                            <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                            <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div className=' w-90 bg-amber-200  shadow-2xl'>
-                    <img className='h-70 rounded-t-2xl w-full object-cover' src={tishrt } alt="" />
-                    <div className='px-3'>
-                        <h3 className='text-xl font-bold mt-4'>Casual Dress 1</h3>
-                    <p className='mt-2'>Fashion never stops. There is always the new project, the new opportunity. The important thing is to take your time and not get stressed. I just want to do what I do.</p>
-                    <div className='flex mt-1 items-center justify-between py-2'>
-                        <p>300$</p>
-                        <div className='flex gap-2'>
-                            <div className='h-4 w-4 rounded-full bg-amber-900'></div>
-                            <div className='h-4 w-4 rounded-full bg-blue-500'></div>
-                            <div className='h-4 w-4 rounded-full bg-orange-600'></div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+                        )
+                    })
+                }
+               
+                
+              
+               
+                
+                
             </div>
         </section>
     </>
